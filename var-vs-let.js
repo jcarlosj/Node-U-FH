146e9633f525c2e0ef3c7a6fe6c0b7d6ee67f880
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2019-08-26 20:15:32 -0500

    [$] Scope: var Vs let (Using Loop)

--- var-vs-let.js
+++ var-vs-let.js
@@ -1,16 +1,14 @@
 /** Repaso JavaScript */
-var xmen = 'Wolverine';
-let hero = 'Batman'
-
-if( true ) {
-    var xmen = 'Magneto';
-    let hero = 'Superman'
+console .group( 'Bucle usando var' );
+for( var i = 0; i < 5; i++) {
+    console .log( `i : ${ i }` );
 }
+console .log( `i > ${ i }` );       /** 6 */
+console .groupEnd();
 
-var xmen = 'Cíclope';           /** Con 'var' se pueden redeclar las variables */
-// let hero = 'Wonder Woman';   /** Con 'let' NO se pueden redeclar las variables (Genera un error por este concepto) */
-
-console .group( 'var Vs let' );
-console .log( 'var >', xmen );
-console .log( 'let >', hero );
+console .group( 'Bucle usando let' );
+for( let j = 0; j < 5; j++) {
+    console .log( `j : ${ j }` );
+}
+// console .log( `j > ${ j }` );   /** Error de referencia ya que 'j' no esta declarado fuera del bucle/ciclo 'for' */
 console .groupEnd();
