350a467bd318d9526f79bbe2d6b177736cad1cdd
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2019-08-26 19:01:19 -0500

    [+] Hello World! en Node

---
+++ app.js
@@ -0,0 +1,6 @@
+let name = 'Luisa Maria';
+
+console .log( 'Hello,', name + ' :P ' );      
+console .log( `Hello, ${ name } !!` );       // Using backticks
+
+// Nota: usando solo el comando 'node' sin el nombre del archivo podemos escribir nuestro codigo para realizar pruebas
