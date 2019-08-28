dc68a12a02802877c237014d9c86101740fa632c
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2019-08-28 16:11:33 -0500

    [+] Arrow Function en Objetos

--- function-arrows.js
+++ function-arrows.js
@@ -1,25 +1,29 @@
 /** Repaso JavaScript */
 
-// Funcion Clasica
-function saludo1( nombre ) {
-    return ` > Hola ${ nombre }! \n   desde una Funcion clasica`;
+// Objeto Implicito
+let Hero = {    
+    nameHero: 'Superman',
+    realFirstName: 'Clark',
+    realLastName: 'Kent',
+    // Declaracion Clasica
+    getNameHero: function() {
+        console.log( ' this: ', this );     // Hace referencia al Objeto Hero
+        return this .nameHero;
+    },
+    // Declaracion Abreviada
+    getRealFirstName() {
+        console.log( ' this: ', this );     // Hace referencia al Objeto Hero
+        return this .realFirstName;
+    },
+    // Declaracion como Function Arrow
+    getRealLastName: () => {
+        console.log( ' this: ', this );     // NO Hace referencia a nada (Tener cuidado con el uso del Arrow Function).
+        return this .realLastName;
+    }
 }
-console .log( saludo1( 'Juliana' ) );
 
-// Funcion Flecha
-let saludo2 = ( nombre ) => {
-    return ` > Hola ${ nombre }! \n   desde una Funcion Flecha`;
-} 
-console .log( saludo2( 'Ana Maria' ) );
-
-// Funcion Flecha abreviada
-let saludo3 = nombre => ` > Hola ${ nombre }! \n   desde una Funcion Flecha Abreviada`;
-console .log( saludo3( 'Elisa Maria' ) );
-
-/** NOTAS: 
- *  1.  Podemos usar o no los parentesis cuando estamos pasando un solo parametro a la funcion 
- *      (comparar lineas 4, 10 y 16). En otros casos siempre debe usarse los parentesis.
- *  2.  Las funciones flecha (Arrow Funtion) son funciones anonimas que se asignan a variables.
- *  3.  Las Arrow Function cuando retornan un valor pueden escribirse en una sola linea eliminando sus
- *      llaves y la palabra reservada 'return'
-*/
+console .group( 'Tipos de Funciones' );
+console .log( ' >>> ', Hero .getNameHero() );         // Superman
+console .log( ' >>> ', Hero .getRealFirstName() );    // Clark
+console .log( ' >>> ', Hero .getRealLastName() );     // Undefined    (No puede obtener el valor ya que 'this' en este metodo no hace referencia al objeto 'Hero')
+console .group();
