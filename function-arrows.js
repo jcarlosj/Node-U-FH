8e1d1a5aebe64ea289ac317b1c806d6da66af1c0
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2019-08-28 15:37:44 -0500

    [+] Arrow Function: Con paso de parametros

--- function-arrows.js
+++ function-arrows.js
@@ -1,24 +1,24 @@
 /** Repaso JavaScript */
 
 // Funcion Clasica
-function saludo1() {
-    return ` > Hola Mundo! \n   desde una Funcion clasica`;
+function saludo1( nombre ) {
+    return ` > Hola ${ nombre }! \n   desde una Funcion clasica`;
 }
-console .log( saludo1() );
+console .log( saludo1( 'Juliana' ) );
 
-// Funcion Flecha: 
-let saludo2 = () => {           
-    return ` > Hola Mundo! \n   desde una Funcion Flecha`;
+// Funcion Flecha
+let saludo2 = ( nombre ) => {
+    return ` > Hola ${ nombre }! \n   desde una Funcion Flecha`;
 } 
-console .log( saludo2() );
+console .log( saludo2( 'Ana Maria' ) );
 
 // Funcion Flecha abreviada
-let saludo3 = _ => ` > Hola Mundo! \n   desde una Funcion Flecha Abreviada`;    
-console .log( saludo3() );
+let saludo3 = nombre => ` > Hola ${ nombre }! \n   desde una Funcion Flecha Abreviada`;
+console .log( saludo3( 'Elisa Maria' ) );
 
 /** NOTAS: 
- *  1.  Podemos dejar los parentesis vacios cuando no deseamos pasar parametros (ver lineas 4 y 10)
- *      Sin embargo tambien podemos usar un _ (guion bajo) que equivale a lo mismo (ver linea 16)
+ *  1.  Podemos usar o no los parentesis cuando estamos pasando un solo parametro a la funcion 
+ *      (comparar lineas 4, 10 y 16). En otros casos siempre debe usarse los parentesis.
  *  2.  Las funciones flecha (Arrow Funtion) son funciones anonimas que se asignan a variables.
  *  3.  Las Arrow Function cuando retornan un valor pueden escribirse en una sola linea eliminando sus
  *      llaves y la palabra reservada 'return'
