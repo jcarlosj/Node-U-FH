81c0a2a41830f07e4f8a809a4eda15c050edd9c4
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2019-08-30 16:38:52 -0500

    [+] Async/Await Encadenadas (Recomendado)

--- async-await.js
+++ async-await.js
@@ -1,29 +1,89 @@
 /** Repaso JavaScript */
 
 // Define una funcion Sincrona (que retorna una Promesa)
-let getUser = () => {
-    return new Promise( ( resolve, reject ) => {
+let people = new Array(
+    {
+        id: 1,
+        firstName: 'Elisa Maria',
+        lastName: 'Giraldo'
+    },
+    {
+        id: 2,
+        firstName: 'Luisa Maria',
+        lastName: 'Balazar'
+    },
+    {
+        id: 3,
+        firstName: 'Ana Maria',
+        lastName: 'Castro'
+    }
+), salaries = [
+    {
+        idUser: 1,
+        value: 1000
+    },
+    {
+        idUser: 2,
+        value: 2000
+    }
+];
 
-        setTimeout( () => {
-            resolve({
-                firstName: 'Juliana',
-                lastName: 'Puerta Villada'
-            });
-        }, 3000 );
+// Funcion que obtiene una persona por ID
+let getPeople = ( id ) => {
+    // Definicion de Promesa (Obtener Persona)          
+    return new Promise( ( resolve, reject ) => {        // resolve y reject: son dos callbacks, adicional al que las contiene. Las promesas son una nueva caracteristica del ES6
+        let personDB = people .find( person => person .id === id );
 
+        // console .log( ' personDB ', personDB );
+        if( ! personDB ) {
+            // reject debe llamarse cuando se asume que no cumplio la tarea, fallo, o se produjo un error.
+            reject( `ERROR: No existe el usuario con el ID: ${ id }` );     
+        }
+        else {
+            // resolve debe llamarse cuando se asume que cumplio la tarea exitosamente. No se pueden mandar mas de dos argumentos.
+            resolve( personDB );                                  
+        }
     });
 }
 
-// Define Funcion Asincrona
-let saludo = async() => {
-    let { firstName: nombre } = await getUser();        // Await: espera por la respuesta de la Promesa retornada por la Funcion Sincrona getUser (Usamos Destructuracion y alias)
+// Funcion que Obtiene el salario de una persona
+let getSalary = ( person ) => {
+    // Definicion de Promesa (Obtener Salario de una Persona)
+    return new Promise( ( resolve, reject ) => {
+        let salaryDB = salaries .find( salary => salary .idUser === person .id );
 
-    return `Hola ${ nombre }!!`;
+        // console .log( ' salaryDB ', salaryDB );
+        if( ! salaryDB ) {
+            // reject debe llamarse cuando se asume que no cumplio la tarea, fallo, o se produjo un error.
+            reject( `ERROR: No se encontro un salario para ${ person .firstName }` );
+        }
+        else {
+            // resolve debe llamarse cuando se asume que cumplio la tarea exitosamente. No se pueden mandar mas de dos argumentos.
+            resolve({           
+                id: person .id,
+                nombre: person .firstName,
+                apellido: person .lastName,
+                salario: salaryDB .value
+            });       
+        }
+    });
 }
 
-// Llamado de ejecucion de la Funcion Asincrona
-saludo() 
-    .then( msg => {
-        console .log( msg );
-    }) 
-    .catch( err => console .log( err ) );
+let getInfo = async ( id ) => {
+    let person = await getPeople( id ),       // 
+        data = await getSalary( person );
+    
+    return `El salario de ${ data .nombre } ${ data .apellido } es de ${ data .salario } usd`;
+}
+
+// USUARIO QUE NO EXISTE
+// Llamada la funcion que retorna el resultado de una promesa para obtener Persona con ID de usuario 10
+getInfo( 10 ) .then( msg => console .log( msg ) ) .catch( err => console .log( err ) );
+
+// USUARIO QUE EXISTE Y TIENE SALARIO ASIGNADO
+// Llamada la funcion que retorna el resultado de una promesa para obtener Persona con ID de usuari
+getInfo( 1 ) .then( msg => console .log( msg ) ) .catch( err => console .log( err ) );
+
+// USUARIO QUE EXISTE Y NO TIENE SALARIO ASIGNADO
+// Llamada la funcion que retorna el resultado de una promesa para obtener Persona con ID de usuario 3    
+getInfo( 3 ) .then( msg => console .log( msg ) ) .catch( err => console .log( err ) );
