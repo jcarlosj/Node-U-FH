f8b157619722db3ef89770c648061c5ebae3cd49
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2019-08-30 18:52:55 -0500

    [$] Refactor: Escribir archivos de forma asincrona desde la linea de comandos

--- app.js
+++ app.js
@@ -2,11 +2,6 @@
 // Importa nuestro archivo 
 const { crearArchivo } = require( './lib/multiply' );       // Destructuracion {}
 
-//console .log( 'Module', module );       // module: Objeto Global que esta disponible durante toda la aplicacion
-//console .log( 'Process', process );         // process: Variable de entorno global
-console .log( 'Process >>', process .argv );  // process .argv: Variable con los argumentos (por defecto siempre trae 2 en un Array)
-                                              // El primero el 'path' de la instalacion de 'node'
-                                              // El segundo el 'path' del archivo ejecutado
 let argv = process .argv,
     parametro = argv[ 2 ],              
     base = parametro .split( '=' )[ 1 ];      // parametro .slit( '=' ) es igual a: [ '--base', '5' ]
@@ -18,13 +13,3 @@ crearArchivo( base )
         console .log( `El archivo ${ file } a sido creado y guardado!` );
     })
     .catch( err => console .log( err) );
-
-/** NOTA:
- *  Ahora podemos enviar en la terminal la orden para generar una tabla específica escribiendo
- *      $ node ./app.js --base=3  
- *          ó
- *      $ node app --base=3  
- *          ó
- *      $ node app -b=3  
- * 
- */
