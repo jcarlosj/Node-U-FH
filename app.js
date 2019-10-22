b69c7e5c76bb3674dfb8a72235a174a0c041d489
Juan Carlos Jiménez Gutierrez <jcjimenez29@misena.edu.co>
2019-10-22 18:10:42 -0500

    [+] NPM: Instala e Implementa 'yargs'
    Aclaracion:
    Yargs te ayuda a crear herramientas interactivas de línea de comandos,
    analizando argumentos y generando una elegante interfaz de usuario.
    
    Testing:
    Compara la forma como process .argv y yargs tratan la linea de comandos
    y la manera como cada uno maneja los parametros pasados a la consola
    
    Probar con los siguientes comandos y ver la estructura de datos de cada uno
     - npm app
     - npm app --base=5
     - npm app listar --base 5

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 /** Bases de Node */
-// Importa nuestro archivo 
-const { crearArchivo } = require( './lib/multiply' );       // Destructuracion {}
+const argvYargs = require( 'yargs' ) .argv,
+      { crearArchivo } = require( './lib/multiply' );       // Destructuracion {}
 
-let argv = process .argv,
-    parametro = argv[ 2 ],              
-    base = parametro .split( '=' )[ 1 ];      // parametro .slit( '=' ) es igual a: [ '--base', '5' ]
+let argv = process .argv;
+    // parametro = argv[ 2 ],              
+    // base = parametro .split( '=' )[ 1 ];      // parametro .slit( '=' ) es igual a: [ '--base', '5' ]
                                               // base es igual a: 5
+console .info( 'argv yargs', argvYargs );
+console .info( 'argv process', argv );
 
 // Llama la funcion que retorna una Promesa
-crearArchivo( base )
-    .then( file => {
-        console .log( `El archivo ${ file } a sido creado y guardado!` );
-    })
-    .catch( err => console .log( err) );
+// crearArchivo( base )
+//     .then( file => {
+//         console .log( `El archivo ${ file } a sido creado y guardado!` );
+//     })
+//     .catch( err => console .log( err) );
